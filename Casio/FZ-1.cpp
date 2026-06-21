9b8aa4d51311f61e0ec85622811d335c84aa3843
stdjon <stdjon.h@gmail.com>
2026-06-21 12:46:04 -0700

    fzutility: remove unneeded \ from end of lines

--- Casio/FZ-1.cpp
+++ Casio/FZ-1.cpp
@@ -8,12 +8,12 @@ template<typename T>struct ReturnVoice { using Type = Voice; };
 template<typename T>struct ReturnVoice<const T> { using Type = const Voice; };
 
 template<typename T>typename ReturnVoice<T>::Type &voice_at(T &v, size_t n) {
-    assert(n < 4); \
-    switch(n) { \
-        default: return v.v1; \
-        case 1: return v.v2; \
-        case 2: return v.v3; \
-        case 3: return v.v4; \
+    assert(n < 4);
+    switch(n) {
+        default: return v.v1;
+        case 1: return v.v2;
+        case 2: return v.v3;
+        case 3: return v.v4;
     }
 }
 
